7691489cb56813c17f7e7153fc14868a778e6dbf
LEGION <121240691+git-legion@users.noreply.github.com>
2023-03-06 17:49:28 +0530

    Update routes.js

--- FrontEnd/routes.js
+++ FrontEnd/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fetch = require('node-fetch')
 const router = express.Router()
 
-const host= 'http://10.128.0.25:8000'
+const host= '127.0.0.1:8000'
 
 router.get('/', (req, res)=>{
     res.render('index')
